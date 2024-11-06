07a10264b36ebcf2fec28826c7f0f16e5bda8b18
themiya-420 <themiya420@gmail.com>
2024-11-06 15:30:44 +0530

    Implemented logic to extact keypoints from feed

--- Scripts/main.py
+++ Scripts/main.py
@@ -22,7 +22,7 @@ def mediapipe_detection(image, model):
 
 # Drawing Landmarks on the image feed
 def draw_landmarks(image, results):
-    #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+    #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_IRISES)
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
@@ -38,14 +38,15 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     
         #make Detections
         image, results = mediapipe_detection(frame, holistic)
-        print(results)
+        #print(results)
 
         # Draw 
         draw_landmarks(image, results)
     
         #showing the feed from the camera
         cv2.imshow('Live Feed', image)
-    
+        #print(results.pose_landmark.landmark)
+
         #breaking the process
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -53,3 +54,13 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     cv2.destroyAllWindows()
     for i in range (1,5):
         cv2.waitKey(1)
+
+
+
+# Extracting KeyPoints
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatanate([pose, lh, rh])
+
